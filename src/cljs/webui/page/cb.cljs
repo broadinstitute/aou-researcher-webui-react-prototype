e81b6e4df66d5bb26d6e9452ebe8fcdcdf8d77d5
David Mohs <dgit@mohsinc.com>
2017-05-11 13:39:17 -0400

    Wrapper component for Vanderbilt CB

---
+++ src/cljs/webui/page/cb.cljs
@@ -0,0 +1,82 @@
+(ns webui.page.cb
+  (:require
+   [dmohs.react :as r]
+   [webui.utils :as u]
+   ))
+
+(defn- xhr-force-send-with-credentials [data]
+  (this-as
+   this
+   (aset this "withCredentials" true)
+   (js-invoke this "_originalSend" data)))
+
+(r/defc CohortBuilder
+  {:render
+   (fn []
+     [:div {:id "ROOT-2521314" :class-name "v-app valo applicationui"
+            :style {:height "90vh"}}
+      [:div {:class-name "v-app-loading"}]])
+   :component-did-mount
+   (fn [{:keys [this]}]
+     (aset js/XMLHttpRequest "prototype" "_originalSend"
+           (aget js/XMLHttpRequest "prototype" "send"))
+     (aset js/XMLHttpRequest "prototype" "send" xhr-force-send-with-credentials)
+     (u/load-script
+      "http://35.185.116.214/pmi-cb/VAADIN/vaadinBootstrap.js?v=7.7.5"
+      #(this :-vaadin-bootstrap-loaded)))
+   :component-will-unmount
+   (fn []
+     (js-invoke (aget js/window "location") "reload"))
+   ;; All of this represents an effort to clean-up after Vaadin, but it still leaves garbage.
+   ;; For now, refreshing the page may be the only option.
+   :-unload-vaadin
+   (fn [{:keys [this]}]
+     (aset js/XMLHttpRequest "prototype" "send"
+           (aget js/XMLHttpRequest "prototype" "_originalSend"))
+     (->
+      (js-invoke js/document "getElementById"
+                 "org.vumc.ori.cohortbuilder.widgetsets.CohortBuilderWidgetSet")
+      (js-invoke "remove"))
+     (-> (js-invoke js/document "getElementById" "ROOT-2521314-overlays")
+         (js-invoke "remove"))
+     (u/unload-script "http://35.185.116.214/pmi-cb/VAADIN/vaadinBootstrap.js?v=7.7.5")
+     (js-delete js/window "vaadin"))
+   :-vaadin-bootstrap-loaded
+   (fn []
+     (when-not (fn? (aget js/window "__gwtStatsEvent"))
+       (aset js/vaadin "gwtStatsEvents" #js[])
+       (aset js/window "__gwtStatsEvent"
+             (fn [e]
+               (js-invoke (aget js/vaadin "gwtStatsEvents") "push" e)
+               true)))
+     (js-invoke
+      js/vaadin "initApplication"
+      "ROOT-2521314"
+      #js{
+          "theme" "cohortbuilder"
+          "versionInfo" {
+                           "vaadinVersion" "7.7.5"
+                           },
+          "widgetset" "org.vumc.ori.cohortbuilder.widgetsets.CohortBuilderWidgetSet",
+          "comErrMsg" {
+                         "caption" "Communication problem",
+                         "message" "Take note of any unsaved data, and <u>click here</u> or press ESC to continue.",
+                         "url" nil
+                         },
+          "authErrMsg" {
+                          "caption" "Authentication problem",
+                          "message" "Take note of any unsaved data, and <u>click here</u> or press ESC to continue.",
+                          "url" nil
+                          },
+          "sessExpMsg" {
+                          "caption" "Session Expired",
+                          "message" "Take note of any unsaved data, and <u>click here</u> or press ESC key to continue.",
+                          "url" nil
+                          },
+          "vaadinDir" "http://35.185.116.214/pmi-cb/VAADIN/",
+          "debug" false,
+          "standalone" true,
+          "heartbeatInterval" 300,
+          "serviceUrl" "http://35.185.116.214/pmi-cb/vaadinServlet/",
+          "browserDetailsUrl" "http://35.185.116.214/pmi-cb/"
+          }))})
